ce67b49ea9743eb8619a2471cfc415d3b39d4d6f
Zheheng Zhang <76390451+zhangzheheng12345@users.noreply.github.com>
2024-01-13 18:05:29 +0800

    Update styles

--- src/Welcome.vue
+++ src/Welcome.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col items-center">
-    <h1 class="m-1.5rem text-3rem mb-30vh">Talkland</h1>
+    <h1 class="m-1.2rem mt-2rem text-3rem text-center">Talkland</h1>
+    <p class="m-1.5rem text-1.2rem mb-25vh h-3rem text-center text-#555">
+      {{ typer }}
+    </p>
     <button
       @click="skipToHome"
       class="flex items-center button p-5px pl-8px pr-6px"
@@ -11,9 +14,36 @@
   </div>
 </template>
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+
+const words = ["Say something.", "Wander.", "Alley.", "Seventh Avenue."];
+
+const sleep = async (delay: number) => {
+  return new Promise((resolve, reject) => setTimeout(resolve, delay));
+};
+
+const typer = ref("");
+
+const typeWords = async () => {
+  let i = 0;
+  const typeWord = async () => {
+    for (let j = 0; j <= words[i].length; j++) {
+      typer.value = words[i].slice(0, j);
+      await sleep(120);
+    }
+  };
+  while (true) {
+    await typeWord();
+    await sleep(750);
+    i++;
+    if (i === words.length) i = 0;
+  }
+};
+
+onMounted(typeWords);
 
 const skipToHome = () => {
   router.push("/home");
